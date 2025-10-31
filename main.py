d91c47d4f26636a45f893a87b6a5dc4919feddf5
Agustin Miranda <agusmiranda1611@gmail.com>
2025-10-31 17:44:13 -0300

    crear menu

---
+++ main.py
@@ -0,0 +1,23 @@
+def mostrar_menu():
+    while True:
+        opcion = int(input("""
+    Bienvenido al programa de gestión de paises!
+    Elija su opción:
+    1. Agregar país
+    2. Actualizar datos de un país
+    3. Buscar un país
+    4. Filtrar países
+    5. Ordenar países
+    6. Mostrar estadísticas
+    7. Salir
+    """))
+        
+        match opcion:
+        
+            case 7:
+                break
+
+
+            
+        
+        
